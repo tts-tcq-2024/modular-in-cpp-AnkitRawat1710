e353727c8286b02d3d7f462fa764b74fce19b3b0
Ankit Rawat <84120496+AnkitRawat1710@users.noreply.github.com>
2024-09-25 14:22:57 +0530

    Update ColorCoding.cpp

--- ColorCoding.cpp
+++ ColorCoding.cpp
@@ -1,11 +1,21 @@
-#ifndef COLORCODING_H
-#define COLORCODING_H
-
-#include "ColorPair.h"
+#include "ColorCoding.h"
 
 namespace TelCoColorCoder {
-    ColorPair GetColorFromPairNumber(int pairNumber);
-    int GetPairNumberFromColor(MajorColor major, MinorColor minor);
-}
+    
+    extern const char* MajorColorNames[];
+    extern const char* MinorColorNames[];
 
-#endif
+    int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+    ColorPair GetColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+    }
+
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major * numberOfMinorColors + minor + 1;
+    }
+}
